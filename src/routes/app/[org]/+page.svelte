473b665da6d83431bc24ddc19d5a4a5a33899407
Jason <jtbernas@gmail.com>
2024-11-26 02:23:19 +0000

    update: trigger ci

--- src/routes/app/[org]/+page.svelte
+++ src/routes/app/[org]/+page.svelte
@@ -1,16 +1,16 @@
 <script lang="ts">
-	import { onMount } from "svelte";
-	import { goto } from "$app/navigation";
+	import { onMount } from 'svelte';
+	import { goto } from '$app/navigation';
 
-	import { LoadingPage } from "$module/page";
+	import { LoadingPage } from '$module/page';
 
-	import { orgTarget } from "$utils/index";
+	import { orgTarget } from '$utils/index';
 
-  onMount(async () => {
+	onMount(async () => {
 		await goto(orgTarget(`/dashboard`) || '#', {
 			invalidateAll: true
-		})
-  });
+		});
+	});
 </script>
 
 <LoadingPage />
